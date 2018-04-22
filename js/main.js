65511de701f135f75106acb83f1c1723928045cb
shuling0 <38003032+shuling0@users.noreply.github.com>
2018-04-22 16:44:57 +0800

    finished

--- js/main.js
+++ js/main.js
@@ -2,6 +2,20 @@
 $(function(){
     $('#owl-demo').owlCarousel({
         items: 1,
-		
     });
+	
 })
+
+function borderq(){
+	var a=0;
+	if(a==0){
+		a+=1;
+		
+		alert(a);
+		}
+	if(a==1){
+		a-=1;
+		alert(a);
+		}
+	}
+	
