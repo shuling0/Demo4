c0e186fe4911c3758c36203f901a3cae55db7abf
shuling0 <38003032+shuling0@users.noreply.github.com>
2018-04-24 18:23:35 +0800

    amend

--- js/main.js
+++ js/main.js
@@ -6,16 +6,31 @@ $(function(){
 	
 })
 
-function borderq(){
-	var a=0;
-	if(a==0){
-		a+=1;
-		
-		alert(a);
-		}
-	if(a==1){
-		a-=1;
-		alert(a);
+function emmm1(){
+	document.getElementById("emm1").style.border="5px solid orange";
+	if(a){
+	document.getElementById("emm1").style.border="5px solid #E2E2E2";
+	}
+	}
+function emmm2(){
+	var a=true;
+	if(a){
+	document.getElementById("emm2").style.border="5px solid orange";
+	a=false;
+	}
+	else{
+		document.getElementById("emm2").style.border="5px solid #E2E2E2";
+		a=true;
 		}
 	}
-	
+function emmm3(){
+	var a=true;
+	if(a){
+	document.getElementById("emm3").style.border="5px solid orange";
+	a=false;
+	}
+	else{
+		document.getElementById("emm3").style.border="5px solid #E2E2E2";
+		a=true;
+		}
+	}
