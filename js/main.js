bd93ded679ed4d4e5d15c4c0450a1488dde44689
shuling0 <38003032+shuling0@users.noreply.github.com>
2018-04-24 18:35:44 +0800

    amend.1

--- js/main.js
+++ js/main.js
@@ -6,31 +6,3 @@ $(function(){
 	
 })
 
-function emmm1(){
-	document.getElementById("emm1").style.border="5px solid orange";
-	if(a){
-	document.getElementById("emm1").style.border="5px solid #E2E2E2";
-	}
-	}
-function emmm2(){
-	var a=true;
-	if(a){
-	document.getElementById("emm2").style.border="5px solid orange";
-	a=false;
-	}
-	else{
-		document.getElementById("emm2").style.border="5px solid #E2E2E2";
-		a=true;
-		}
-	}
-function emmm3(){
-	var a=true;
-	if(a){
-	document.getElementById("emm3").style.border="5px solid orange";
-	a=false;
-	}
-	else{
-		document.getElementById("emm3").style.border="5px solid #E2E2E2";
-		a=true;
-		}
-	}
